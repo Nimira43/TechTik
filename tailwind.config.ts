a227a7b3ab673c319078c44bfa0b9ed62536cf25
Nimira43 <n.rabone72@aol.com>
2024-10-07 23:06:07 +0100

    Using Tailwind to do some styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("tailwindcss-animate")],
     theme: {
     	extend: {
