7b07a89ec144f63813874f2565f5943c4b45d5fd
Nimira43 <n.rabone72@aol.com>
2024-10-07 23:16:18 +0100

    Brought in Shadcn  darkmode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import MainNav from "@/components/MainNav"
+import { ThemeProvider } from "@/components/theme-provider"
 
 export const metadata: Metadata = {
   title: "TechTik",
@@ -15,16 +16,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
-          <div className="max-w-6xl w-full">
-            <MainNav />  
-          </div>   
-        </nav>
-        <main className="flex flex-col items-center">
-          <div className="max-w-6xl w-full">
-            {children}
-          </div>
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
+            <div className="max-w-6xl w-full">
+              <MainNav />  
+            </div>   
+          </nav>
+          <main className="flex flex-col items-center">
+            <div className="max-w-6xl w-full">
+              {children}
+            </div>
+          </main>
+        </ThemeProvider>
+        
       </body>
     </html>
   )
