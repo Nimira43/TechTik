e89eb37e53d1541ed75634104208623f2ba2254f
Nimira43 <n.rabone72@aol.com>
2024-10-07 23:21:27 +0100

    Created ToggleMode.tsx component to handle switching light / dark modes

---
+++ components/ToggleMode.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const ToggleMode = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ToggleMode
