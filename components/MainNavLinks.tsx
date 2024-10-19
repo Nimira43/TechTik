ac216be6293ca0bd62baa52069bf01c02ef9ad7b
Nimira43 <n.rabone72@aol.com>
2024-10-19 17:24:39 +0100

    Highlighting current page in navbar

--- components/MainNavLinks.tsx
+++ components/MainNavLinks.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const MainNavLinks = () => {
@@ -9,11 +10,19 @@ const MainNavLinks = () => {
     {label: 'Tickets', href: '/tickets'},
     {label: 'Users', href: '/users'}
   ]
+  const currentPath = usePathname()
 
   return (
     <div className='flex items-center gap-2'>
       {links.map((link) => (
-        <Link href={link.href} className='navbar-link' key={link.label}>
+        <Link 
+          href={link.href} 
+          className={`navbar-link ${
+            currentPath == link.href && 
+            'cursor-default text-primary/70 hover:text-primary/60'
+          }`} 
+          key={link.label}
+        >
           {link.label}
         </Link>
       ))}      
